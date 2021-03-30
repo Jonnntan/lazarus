0f8051c58feb56d9e3dc38e59be06d990c6e4260
Jonathon Tan <jontan@sephora.sg>
2021-03-30 10:28:32 +0800

    Add admin_request_spec

--- spec/requests/admin_request_spec.rb
+++ spec/requests/admin_request_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Admins", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe '#index' do
+    it 'display Products & Brands link' do
+      get '/'
+      expect(response).to have_http_status(200)
+    end
   end
 end
