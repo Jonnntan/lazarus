290e0f1c29918769916a52fbe98e69b6710bfed6
Jonathon Tan <jontan@sephora.sg>
2021-03-30 15:23:42 +0800

    Update product and brand model with specs"

--- spec/models/products/product_spec.rb
+++ spec/models/products/product_spec.rb
@@ -11,8 +11,7 @@ describe Product do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:brand) }
     it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
   end
-
-
 end
