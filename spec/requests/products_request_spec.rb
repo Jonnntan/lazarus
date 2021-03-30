f7dcae464a45f78ca9ef709462b0f36d49c2a05b
Jonathon Tan <jontan@sephora.sg>
2021-03-30 11:18:23 +0800

    Update: use let instead of before

--- spec/requests/products_request_spec.rb
+++ spec/requests/products_request_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe 'Products', type: :request do
 
   let(:brand) { create(:brand) }
   let!(:product_brand) { create(:product, brand: brand) }
-
-  before do
-    @products_params = attributes_for(:product) do |pd|
+  let!(:product_params) do
+    attributes_for(:product) do |pd|
       pd[:brand_id] = brand.id
     end
   end
@@ -14,6 +13,7 @@ RSpec.describe 'Products', type: :request do
   describe '#index' do
     it 'list all Products' do
       get '/products'
+
       expect(response).to have_http_status(200)
       expect(response.body).to include('Test product 1')
     end
@@ -21,7 +21,7 @@ RSpec.describe 'Products', type: :request do
 
   describe '#create' do
     it 'creates a Product and redirects to the show page' do
-      post '/products', params: { product: @products_params }
+      post '/products', params: { product: product_params }
 
       expect(response).to redirect_to product_path(brand.products.last.id)
       follow_redirect!
@@ -31,7 +31,7 @@ RSpec.describe 'Products', type: :request do
 
   describe '#update' do
     it 'update a Product details and redirects to the show page' do
-      put "/products/#{product_brand.id}", params: { product: @products_params }
+      put "/products/#{product_brand.id}", params: { product: product_params }
 
       expect(response).to redirect_to product_path(brand.products.last.id)
       follow_redirect!
@@ -49,7 +49,7 @@ RSpec.describe 'Products', type: :request do
   describe '#search' do
     it 'search Products by title' do
       get '/search?search=test+product'
-      
+
       expect(response.body).to include('Test product 9')
       expect(response).to have_http_status(200)
     end
